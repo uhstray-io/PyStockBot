cd395df209480914350cc762a48b645cdbff2ef1
Jacob <jacobedwardhaig@gmail.com>
2024-07-17 21:31:34 -0400

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyStockBot
  
+## Installation
+```bash
+pip install scikit-learn pandas numpy matplotlib xgboost yfinance seaborn
+```
---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+if __name__ == "__main__":
+    print("Hello World")
